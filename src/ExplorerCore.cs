a0595c616eab80aa00552099f4245b1c5aaaba51
ASTerisk Games <121459020+elidianesampaiotrabuco@users.noreply.github.com>
2024-12-27 15:22:57 -0300

    Bump version (first commit)

--- src/ExplorerCore.cs
+++ src/ExplorerCore.cs
@@ -19,8 +19,8 @@ namespace UnityExplorer;
 public static class ExplorerCore
 {
     public const string NAME = "UnityExplorer";
-    public const string VERSION = "4.12.2";
-    public const string AUTHOR = "Sinai, yukieiji";
+    public const string VERSION = "4.13.0";
+    public const string AUTHOR = "Sinai, yukieiji, freddyguythe10th, ASTerisk";
     public const string GUID = "com.sinai.unityexplorer";
 
     public static IExplorerLoader Loader { get; private set; }
